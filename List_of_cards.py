3a2c6d4fbaadb19175ab6e9c61a458451e5fc83a
Mila123s <86156114+Mila123s@users.noreply.github.com>
2021-06-22 10:29:13 +0200

    Update List_of_cards.py

--- List_of_cards.py
+++ List_of_cards.py
@@ -1,15 +1,12 @@
-alle=[]
+alle_sets=[]   #wordt de lijst met alle 81 kaarten 
 kleur=['green', 'purple', 'red']
 vorm=['diamond','oval', 'squigle']
 binnenkant=['empty', 'shaded','filled']
 hoeveelheid=[1,2,3]
-set_kaarten=[]
+set_kaarten=[]   #wordt een lijst met telkens 1 kaart erin 
 for i in kleur:
     for j in vorm:
         for k in binnenkant:
             for l in hoeveelheid:
                 set_kaarten=[i,j,k,l]
-                alle.append(set_kaarten)
-
-print(alle)
-print(len(alle))
+                alle_sets.append(set_kaarten)
