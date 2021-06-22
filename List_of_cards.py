99a9d1411a954e3dbaf2c461b528e24bf0fb5355
Mila123s <86156114+Mila123s@users.noreply.github.com>
2021-06-22 10:37:21 +0200

    Update List_of_cards.py

--- List_of_cards.py
+++ List_of_cards.py
@@ -8,8 +8,8 @@ hoeveelheid=[1,2,3]
 kaart=[]   #wordt een lijst met telkens 1 kaart erin 
 for i in kleur:
     for j in vorm:
-        for k in binnenkant:
-            for l in inhoud:
+        for k in inhoud:
+            for l in hoeveelheid:
                 kaart=[i,j,k,l]
                 alle_kaarten.append(kaart)
 random.shuffle(alle_kaarten)   #zet de kaarten in een random volgorde in de lijst, is vergelijkbaar met schudden van een stapel kaarten   
