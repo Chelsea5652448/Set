9fad2ade0b7db203b27e6df47a172061f4e24276
Chelsea5652448 <86156060+Chelsea5652448@users.noreply.github.com>
2021-06-22 10:21:52 +0200

    Update set_tegen_de_computer.py

--- set_tegen_de_computer.py
+++ set_tegen_de_computer.py
@@ -1,1 +1,15 @@
-print("Dit is onze code die het spel set simuleert")
+alle=[]
+kleur=['green', 'purple', 'red']
+vorm=['diamond','oval', 'squigle']
+binnenkant=['empty', 'shaded','filled']
+hoeveelheid=[1,2,3]
+set_kaarten=[]
+for i in kleur:
+    for j in vorm:
+        for k in binnenkant:
+            for l in hoeveelheid:
+                set_kaarten=[i,j,k,l]
+                alle.append(set_kaarten)
+
+print(alle)
+print(len(alle))
